dc3269022083df990487a9bac255da0cf156111e
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-18 22:57:26 +0530

    create file s
    keyword and variable

---
+++ keyword.py
@@ -0,0 +1,41 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Keywords:->
+>>> #Keywords are predefine word/fixed / reserved
+>>> #Because They have some special meaning
+>>> 
+>>> #In python we have 35 keywores
+>>> 
+>>> #Instead of 35 keywores we have 3 special keywords
+>>> # Specail Keywords: True / False / None
+>>> #1. Uppercase
+>>> #2. We can assign special keyword to a variable
+>>> 
+>>> 
+>>> 
+>>> #if you have to fetch all keywords
+>>> # Group
+>>> # help("keywords")
+>>> 
+>>> help('keywords')
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+
+False               class               from                or
+None                continue            global              pass
+True                def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+break               for                 not                 
+
+
+#List format
+#import keyword
+#keyword.kwlist
+
+import keyword
+keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
---
+++ variable.py
@@ -0,0 +1,32 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Variable:
+>>> 
+>>> # A variable is container that holds the value or information
+>>> 
+>>> # variableName=value
+>>> 
+>>> #Its name given to memeory location with actual value
+>>> 
+>>> 
+>>> #Rules:-
+>>> 
+>>> #1. in place variable we can use uppercase /lowercase character
+>>> #2. we can't use specaial character execpt '_'
+>>> #3. we can't use number but we inintalize in between or end
+>>> #4. we can pass unlimited character but according to
+>>> #   PEP8(Python Enhancement Proropsal 8-version) 79 characters
+>>> 
+>>> 
+>>> 
+>>> #Style writitn varaible
+>>> 
+>>> # 3 types
+>>> #1. snake case :->  hello_world :->  if we have more than one variable to psepate that variable we use '_'
+
+#2. camel case:-> helloWorld:-> the first word in lowercase but second word start with uppercase
+
+#3.Pascal case:-> HelloWorld:-> firstword-> uppercase second word:-> uppercase
+
+
+
