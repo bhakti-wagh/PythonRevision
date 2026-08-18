13f5c10b652e3dd1b14e0fd7c54ba5a0b573bc73
bhakti wagh <157628032+bhakti-wagh@users.noreply.github.com>
2026-08-18 23:12:31 +0530

    Create type_id.py

---
+++ type_id.py
@@ -0,0 +1,33 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #type()
+>>> 
+>>> #type() is inbuilt function
+>>> #if we want to check which type of data we store in variable
+>>> 
+>>> 
+>>> a=10
+>>> 
+>>> type(a)
+<class 'int'>
+>>> 
+>>> type(45.5)
+<class 'float'>
+>>> 
+>>> 
+>>> 
+>>> #id()
+>>> 
+>>> #id():-> inbuilt function
+>>> # if we want to fetch address of variable
+>>> 
+>>> #id(varname)
+>>> 
+>>> id(a)
+140737325909192
+
+
+
+b=10
+id(b)
+140737325909192
